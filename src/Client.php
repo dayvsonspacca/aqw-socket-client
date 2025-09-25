ed69a7a38de3e49fc482098ec06ce1d3d85959c6
Dayvson Spacca <spacca.dayvson@gmail.com>
2025-09-25 20:52:03 -0300

    feat: adds basic tcp listener for Client

--- src/Client.php
+++ src/Client.php
@@ -9,4 +9,39 @@ namespace AqwSocketClient;
  */
 class Client
 {
+    private $socket = null;
+
+    public function __construct(private readonly Server $server) {}
+
+    public function connect()
+    {
+        $this->socket = @stream_socket_client(
+            "tcp://{$this->server->hostname}:{$this->server->port}",
+            $errno,
+            $errstr,
+            5
+        );
+
+        if (!$this->socket) {
+            echo "Erro: $errstr ($errno)\n";
+            return;
+        }
+
+        stream_set_blocking($this->socket, false);
+
+        while (!feof($this->socket)) {
+            $data = fgets($this->socket, 1024);
+            if ($data) {
+                $this->onData($data);
+            }
+            usleep(100000);
+        }
+
+        fclose($this->socket);
+    }
+
+    private function onData(string $mensagem)
+    {
+        echo "[RAW] $mensagem\n";
+    }
 }
