6d3360466cde73fefa7fc42da039cb6b75f1d9df
Dayvson Spacca <spacca.dayvson@gmail.com>
2025-11-07 17:40:40 -0300

    feat: command to load player

---
+++ src/Commands/LoadPlayerCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AqwSocketClient\Commands;
+
+use AqwSocketClient\Interfaces\CommandInterface;
+use AqwSocketClient\Packet;
+
+class LoadPlayerCommand implements CommandInterface
+{
+    public function __construct(
+        public readonly int $areaId,
+        public readonly int $userId
+    ) {}
+
+    public function pack(): Packet
+    {
+        return Packet::packetify(
+            "%xt%zm%retrieveUserDatas%{$this->areaId}%{$this->userId}%"
+        );
+    }
+}
