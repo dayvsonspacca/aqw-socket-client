6b724742da0a7299f04ba68fd474fdfcc381066d
Dayvson Spacca <spacca.dayvson@gmail.com>
2025-11-10 17:17:27 -0300

    feat: add the player area id in global listerner

--- src/Listeners/GlobalPlayerListener.php
+++ src/Listeners/GlobalPlayerListener.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace AqwSocketClient\Listeners;
 
+use AqwSocketClient\Events\JoinedAreaEvent;
 use AqwSocketClient\Events\LoginResponseEvent;
 use AqwSocketClient\Interfaces\EventInterface;
 use AqwSocketClient\Interfaces\ListenerInterface;
@@ -11,11 +12,16 @@ use AqwSocketClient\Interfaces\ListenerInterface;
 class GlobalPlayerListener implements ListenerInterface
 {
     public int $socketId;
+    public int $areaId;
 
     public function listen(EventInterface $event)
     {
         if ($event instanceof LoginResponseEvent) {
             $this->socketId = $event->socketId;
+        }
+
+        if ($event instanceof JoinedAreaEvent) {
+            $this->areaId = $event->areaId;
         }
     }
 }
