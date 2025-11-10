ee45dc4d0e102cb2b00610dbfe013ca71fe6e6ba
Dayvson Spacca <spacca.dayvson@gmail.com>
2025-11-10 16:37:58 -0300

    feat: adds load player command, util for loading player

---
+++ src/Commands/LoadPlayerInventoryCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AqwSocketClient\Commands;
+
+use AqwSocketClient\Interfaces\CommandInterface;
+use AqwSocketClient\Packet;
+
+class LoadPlayerInventoryCommand implements CommandInterface
+{
+    public function __construct(
+        public readonly int $areaId,
+        public readonly int $socketId
+    ) {}
+
+    public function pack(): Packet
+    {
+        return Packet::packetify(
+            "%xt%zm%retrieveInventory%{$this->areaId}%{$this->socketId}%"
+        );
+    }
+}
