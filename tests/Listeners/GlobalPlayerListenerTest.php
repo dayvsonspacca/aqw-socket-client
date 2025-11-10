bd82545204c4f40ca342a4c17d6005315fbda8aa
Dayvson Spacca <spacca.dayvson@gmail.com>
2025-11-10 17:55:54 -0300

    tests(GlobalPlayerListener): add tests

---
+++ tests/Listeners/GlobalPlayerListenerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AqwSocketClient\Tests\Listeners;
+
+use AqwSocketClient\Events\JoinedAreaEvent;
+use AqwSocketClient\Events\LoginResponseEvent;
+use AqwSocketClient\Listeners\GlobalPlayerListener;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class GlobalPlayerListenerTest extends TestCase
+{
+    private GlobalPlayerListener $listener;
+
+    protected function setUp(): void
+    {
+        $this->listener = new GlobalPlayerListener();
+    }
+
+    #[Test]
+    public function should_listen_to_login_response_event_and_update_socket_id()
+    {
+        $event = new LoginResponseEvent(true, 5);
+        
+        $this->assertFalse(isset($this->listener->socketId));
+        
+        $this->listener->listen($event);
+
+        $this->assertSame($this->listener->socketId, 5);
+    }
+
+    #[Test]
+    public function should_listen_to_joined_area_event_and_update_area_id()
+    {
+        $event = new JoinedAreaEvent('yulgar', 2, 6, []);
+        
+        $this->assertFalse(isset($this->listener->areaId));
+        
+        $this->listener->listen($event);
+
+        $this->assertSame($this->listener->areaId, 6);
+    }
+}
