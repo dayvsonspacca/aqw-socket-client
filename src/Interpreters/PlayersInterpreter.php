04a6c624437dcfc9f6ecdd7911e215cffc13a084
Dayvson Spacca <spacca.dayvson@gmail.com>
2025-11-10 15:33:10 -0300

    chore(PlayersInterpreter): add a respective interpret function to each message type.

--- src/Interpreters/PlayersInterpreter.php
+++ src/Interpreters/PlayersInterpreter.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace AqwSocketClient\Interpreters;
 
 use AqwSocketClient\Enums\{DelimitedMessageType, JsonCommandType};
-use AqwSocketClient\Events\{MovedToAreaEvent, PlayerDetectedEvent};
+use AqwSocketClient\Events\{PlayerDetectedEvent};
 use AqwSocketClient\Interfaces\{InterpreterInterface, MessageInterface};
-use AqwSocketClient\Messages\{DelimitedMessage, JsonCommand, JsonMessage};
+use AqwSocketClient\Messages\{DelimitedMessage};
 
 /**
  * An interpreter responsible for parsing incoming server messages that are
@@ -17,10 +17,9 @@ use AqwSocketClient\Messages\{DelimitedMessage, JsonCommand, JsonMessage};
  */
 class PlayersInterpreter implements InterpreterInterface
 {
-    public function __construct(
-        public readonly string $username
-    ) {
-    }
+    /** @var EventInterface[] $events */
+    private array $events = [];
+
     /**
      * Currently handles:
      * - **ExitArea** delimited messages (for player detection, based on your logic).
@@ -33,35 +32,18 @@ class PlayersInterpreter implements InterpreterInterface
      */
     public function interpret(MessageInterface $message): array
     {
-        $events = [];
+        $this->interpretDelimited($message);
 
-        if ($message instanceof DelimitedMessage) {
-            if ($message->type === DelimitedMessageType::ExitArea && count($message->data) === 2) {
-                $events[] = new PlayerDetectedEvent($message->data[1]);
-            }
-            if ($message->type === DelimitedMessageType::PlayerChange) {
-                $events[] = new PlayerDetectedEvent($message->data[0]);
-            }
-        } elseif ($message instanceof JsonMessage) {
-            $commands = array_filter($message->commands, fn (JsonCommand $command) => $command->type === JsonCommandType::MoveToArea);
-            foreach ($commands as $command) {
-                if ($command->type === JsonCommandType::MoveToArea) {
-                    $players = array_map(fn ($data) => $data['strUsername'], $command->data['uoBranch']);
-                    $user    = array_values(array_filter($command->data['uoBranch'], fn ($player) => $player['strUsername'] === $this->username));
-                    if (empty($user)) {
-                        continue;
-                    }
+        return $this->events;
+    }
 
-                    $events[] = new MovedToAreaEvent(
-                        areaId: $command->data['areaId'],
-                        players: $players,
-                        userId: $user[0]['entID'],
-                        mapName: $command->data['strMapName']
-                    );
-                }
-            }
+    private function interpretDelimited(DelimitedMessage $message)
+    {
+        if ($message->type === DelimitedMessageType::ExitArea) {
+            $this->events[] = new PlayerDetectedEvent($message->data[1]);
         }
-
-        return $events;
+        if ($message->type === DelimitedMessageType::PlayerChange) {
+            $this->events[] = new PlayerDetectedEvent($message->data[0]);
+        }
     }
 }
