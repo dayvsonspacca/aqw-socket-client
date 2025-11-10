2bc185c7414be8768c7c72a60434d69f777840ba
Dayvson Spacca <spacca.dayvson@gmail.com>
2025-11-10 15:27:47 -0300

    chore(LoginInterpreter): add a respective interpret function to each message type

--- src/Interpreters/LoginInterpreter.php
+++ src/Interpreters/LoginInterpreter.php
@@ -17,6 +17,9 @@ use AqwSocketClient\Messages\{DelimitedMessage, XmlMessage};
  */
 class LoginInterpreter implements InterpreterInterface
 {
+    /** @var EventInterface[] $events */
+    private array $events = [];
+
     /**
      * Attempts to convert a server message (XML or Delimited) into
      * relevant events during the connection and login phase.
@@ -26,22 +29,27 @@ class LoginInterpreter implements InterpreterInterface
      * - The **LoginResponse** delimited message, resulting in a {@see AqwSocketClient\Events\LoginResponseEvent}.
      *
      * @param MessageInterface $message The raw, uninterpreted message object.
-     * @return array The list of {@see AqwSocketClient\Interfaces\EventInterface} objects generated from the message.
+     * @return EventInterface[] The list of {@see AqwSocketClient\Interfaces\EventInterface} objects generated from the message.
      */
     public function interpret(MessageInterface $message): array
     {
-        $events = [];
+        $this->interpretXml($message);
+        $this->interpretDelimited($message);
 
-        if ($message instanceof XmlMessage) {
-            if ($message->dom->firstChild?->nodeName === 'cross-domain-policy') {
-                $events[] = new ConnectionEstabilishedEvent();
-            }
-        } elseif ($message instanceof DelimitedMessage) {
-            if ($message->type === DelimitedMessageType::LoginResponse) {
-                $events[] = new LoginResponseEvent((bool) $message->data[0] ?? false);
-            }
+        return $this->events;
+    }
+
+    private function interpretXml(XmlMessage $message)
+    {
+        if ($message->dom->firstChild?->nodeName === 'cross-domain-policy') {
+            $this->events[] = new ConnectionEstabilishedEvent();
         }
+    }
 
-        return $events;
+    private function interpretDelimited(DelimitedMessage $message)
+    {
+        if ($message->type === DelimitedMessageType::LoginResponse) {
+            $this->events[] = new LoginResponseEvent((bool) $message->data[0] ?? false);
+        }
     }
 }
