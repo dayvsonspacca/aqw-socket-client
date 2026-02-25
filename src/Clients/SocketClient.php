aa59210caae4c362376d895ea79d1b1ed56e109f
Dayvson Spacca <spacca.dayvson@gmail.com>
2026-02-25 11:48:11 -0300

    refactor: add ClientInterface to standardize communication with AQW servers

---
+++ src/Clients/SocketClient.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AqwSocketClient\Clients;
+
+use AqwSocketClient\Configuration;
+use AqwSocketClient\Interfaces\ClientInterface;
+use RuntimeException;
+use Socket;
+
+final class SocketClient implements ClientInterface
+{
+    private ?Socket $socket = null;
+
+    public function __construct(public readonly Configuration $configuration)
+    {
+    }
+
+    public function connect(): void
+    {
+        if ($this->isConnected()) {
+            throw new RuntimeException('Already connected.');
+        }
+
+        $server = $this->configuration->server;
+
+        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        if ($socket === false) {
+            throw new RuntimeException(
+                'Failed to create socket: ' . socket_strerror(socket_last_error())
+            );
+        }
+
+        if (!socket_connect($socket, $server->hostname, $server->port)) {
+            $error = socket_strerror(socket_last_error($socket));
+            socket_close($socket);
+
+            throw new RuntimeException(
+                'Failed to connect: ' . $error
+            );
+        }
+
+        $this->socket = $socket;
+    }
+
+    public function disconnect(): void
+    {
+        if (!$this->isConnected()) {
+            throw new RuntimeException('Not connected.');
+        }
+
+        socket_close($this->socket);
+
+        $this->socket = null;
+    }
+
+    public function isConnected(): bool
+    {
+        return $this->socket !== null;
+    }
+
+    public function __destruct()
+    {
+        if ($this->isConnected()) {
+            socket_close($this->socket);
+        }
+    }
+}
---
+++ tests/Clients/SocketClientTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AqwSocketClient\Tests\Clients;
+
+use AqwSocketClient\Clients\SocketClient;
+use AqwSocketClient\Configuration;
+use AqwSocketClient\Interfaces\ClientInterface;
+use AqwSocketClient\Server;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+
+final class SocketClientTest extends TestCase
+{
+    private ClientInterface $client;
+    private Configuration $configuration;
+
+    protected function setUp(): void
+    {
+        $this->configuration = Configuration::make(Server::espada());
+        $this->client = new SocketClient($this->configuration);
+    }
+
+    #[Test]
+    public function it_creates_client_with_config()
+    {
+        $this->assertInstanceOf(SocketClient::class, $this->client);
+        $this->assertSame($this->client->configuration, $this->configuration);
+    }
+
+    #[Test]
+    public function it_starts_disconnected(): void
+    {
+        $this->assertFalse($this->client->isConnected());
+    }
+
+    #[Test]
+    public function it_throw_erro_when_try_disconnect_and_its_already_disconnected(): void
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Not connected.');
+
+        $this->client->disconnect();
+    }
+}
