d73f74a5efbe34cf66566a824659cde584963b48
Dayvson Spacca <spacca.dayvson@gmail.com>
2026-02-25 11:53:25 -0300

    refactor: move tests to Unit namespace

---
+++ tests/Unit/Events/ShopLoadedEventTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AqwSocketClient\Tests\Unit\Commands;
+
+use AqwSocketClient\Events\ShopLoadedEvent;
+use AqwSocketClient\Objects\Shop;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class ShopLoadedEventTest extends TestCase
+{
+    private readonly ShopLoadedEvent $event;
+
+    protected function setUp(): void
+    {
+        $shop = $this->createMock(Shop::class);
+        $this->event = new ShopLoadedEvent($shop);
+    }
+
+    #[Test]
+    public function should_create_shop_loaded_event(): void
+    {
+        $this->assertInstanceOf(ShopLoadedEvent::class, $this->event);
+        $this->assertInstanceOf(Shop::class, $this->event->shop);
+    }
+}
---
+++ tests/Unit/Listeners/GlobalPlayerListenerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AqwSocketClient\Tests\Unit\Listeners;
+
+use AqwSocketClient\Events\AreaJoinedEvent;
+use AqwSocketClient\Events\LoginRespondedEvent;
+use AqwSocketClient\Listeners\GlobalPlayerListener;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class GlobalPlayerListenerTest extends TestCase
+{
+    private GlobalPlayerListener $listener;
+
+    protected function setUp(): void
+    {
+        $this->listener = new GlobalPlayerListener();
+    }
+
+    #[Test]
+    public function should_listen_to_login_response_event_and_update_socket_id()
+    {
+        $event = new LoginRespondedEvent(true, 5);
+
+        $this->assertFalse(isset($this->listener->socketId));
+
+        $this->listener->listen($event);
+
+        $this->assertSame($this->listener->socketId, 5);
+    }
+
+    #[Test]
+    public function should_listen_to_joined_area_event_and_update_area_id()
+    {
+        $event = new AreaJoinedEvent('yulgar', 2, 6, [], []);
+
+        $this->assertFalse(isset($this->listener->areaId));
+
+        $this->listener->listen($event);
+
+        $this->assertSame($this->listener->areaId, 6);
+    }
+}
