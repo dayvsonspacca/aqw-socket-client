ceba8d6e6d32d08fe136eb386f7afb32d70aba00
Dayvson Spacca <spacca.dayvson@gmail.com>
2026-02-26 23:07:23 -0300

    feat: introduce AbstractClient for separte run script from SocketClient

--- src/Clients/SocketClient.php
+++ src/Clients/SocketClient.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 namespace AqwSocketClient\Clients;
 
-use AqwSocketClient\Interfaces\ClientInterface;
-use AqwSocketClient\Interfaces\EventInterface;
 use AqwSocketClient\Interfaces\MessageInterface;
-use AqwSocketClient\Interfaces\ScriptInterface;
 use AqwSocketClient\Interfaces\SocketInterface;
 use AqwSocketClient\Messages\DelimitedMessage;
 use AqwSocketClient\Messages\JsonMessage;
@@ -21,7 +18,7 @@ use RuntimeException;
 /**
  * @mago-ignore analyzer:unhandled-thrown-type
  */
-final class SocketClient implements ClientInterface
+final class SocketClient extends AbstractClient
 {
     private bool $connected = false;
 
@@ -99,45 +96,6 @@ final class SocketClient implements ClientInterface
 
         $data = $packet->unpacketify();
         $this->socket->send($data);
-    }
-
-    #[Override]
-    public function run(ScriptInterface $script): void
-    {
-        while ($this->isConnected() && !$script->isDone()) {
-            foreach ($this->receive() as $message) {
-                $this->processMessage($script, $message);
-            }
-        }
-    }
-
-    private function processMessage(ScriptInterface $script, MessageInterface $message): void
-    {
-        foreach ($this->resolveEvents($script, $message) as $event) {
-            $this->dispatchEvent($script, $event);
-        }
-    }
-
-    private function resolveEvents(ScriptInterface $script, MessageInterface $message): array
-    {
-        $events = [];
-
-        foreach ($script->handles() as $eventClass) {
-            $event = $eventClass::from($message);
-
-            if ($event !== null) {
-                $events[] = $event;
-            }
-        }
-
-        return $events;
-    }
-
-    private function dispatchEvent(ScriptInterface $script, EventInterface $event): void
-    {
-        foreach ($script->handle($event) as $command) {
-            $this->send($command->pack());
-        }
     }
 
     #[Override]
