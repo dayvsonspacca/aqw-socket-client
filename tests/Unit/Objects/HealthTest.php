64f614b5708cfda177b80fd1956c4f55b6e4897a
Dayvson Spacca <spacca.dayvson@gmail.com>
2026-02-27 12:34:34 -0300

    tests: covers Health

---
+++ tests/Unit/Objects/HealthTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AqwSocketClient\Tests\Unit\Objects;
+
+use AqwSocketClient\Objects\Health;
+use InvalidArgumentException;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class HealthTest extends TestCase
+{
+    #[Test]
+    public function it_can_create_identifier(): void
+    {
+        $identifier = new Health(200);
+
+        $this->assertInstanceOf(Health::class, $identifier);
+        $this->assertSame($identifier->value, 200);
+    }
+
+    #[Test]
+    public function should_throw_exeception_when_value_equal_zero(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        new Health(-11);
+    }
+}
