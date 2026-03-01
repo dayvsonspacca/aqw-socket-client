072b18ea1e5a5af046a9d34a1ab8008452f67084
Dayvson Spacca <spacca.dayvson@gmail.com>
2026-03-01 00:15:00 -0300

    feat: update JoinAreaCommand to use objects

--- src/Commands/JoinAreaCommand.php
+++ src/Commands/JoinAreaCommand.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace AqwSocketClient\Commands;
 
 use AqwSocketClient\Interfaces\CommandInterface;
+use AqwSocketClient\Objects\Identifiers\RoomIdentifier;
+use AqwSocketClient\Objects\Names\AreaName;
+use AqwSocketClient\Objects\Names\PlayerName;
 use AqwSocketClient\Packet;
 use Override;
 
@@ -18,24 +21,19 @@ use Override;
  */
 final class JoinAreaCommand implements CommandInterface
 {
-    /**
-     * @param string $username The username of the player attempting to join the map.
-     * @param string $mapName The base name of the map to join (e.g., 'battleon', 'yulgar').
-     * @param int $room The specific instance number of the map to join (e.g., 0 for public, 1, 2...). Defaults to 0.
-     */
     public function __construct(
-        public readonly string $username,
-        public readonly string $mapName,
-        public readonly int $room = 0,
+        public readonly PlayerName $playerName,
+        public readonly AreaName $areaName,
+        public readonly ?RoomIdentifier $room = null,
     ) {}
 
     #[Override]
     public function pack(): Packet
     {
         return Packet::packetify(
-            $this->room > 0
-                ? "%xt%zm%cmd%1%tfer%{$this->username}%{$this->mapName}-{$this->room}%"
-                : "%xt%zm%cmd%1%tfer%{$this->username}%{$this->mapName}%",
+            $this->room === null
+                ? "%xt%zm%cmd%1%tfer%{$this->playerName}%{$this->areaName}%"
+                : "%xt%zm%cmd%1%tfer%{$this->playerName}%{$this->areaName}-{$this->room}%",
         );
     }
 }
